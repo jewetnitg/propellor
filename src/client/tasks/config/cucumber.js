1cb3c4a84db99f069a6aed582019a84341a6d014
Rik Hoffbauer <Rikhoffbauer@gmail.com>
2015-05-08 00:27:19 +0200

    wip

--- src/client/tasks/config/cucumber.js
+++ src/client/tasks/config/cucumber.js
@@ -1,14 +1,24 @@
 /**
  * Created by RikHoffbauer on 07/05/15.
  */
-
-var cucumber = require('cucumber');
+// TODO: use cucumber Cli manually (thourhg Cucumber.Cli ?)
+var cucumber = require('gulp-cucumber');
 
 module.exports = function (gulp, plugins, growl) {
   gulp.task('cucumber:client', function () {
     // this shell command works from root of project: cucumber-js -r src/client/features/ src/client/features/
-    return gulp.src('./src/client/features/')
-      .pipe(cucumber())
+
+    var cwd = process.cwd();
+    process.chdir(cwd + '/src/client');
+
+    return gulp.src('./src/client/features/*')
+      //.pipe(cucumber({
+      //  support: '*support/**/*.js',
+      //  steps: '*step_definitions/**/*.js',
+      //  format: 'pretty'
+      //}))
+      .pipe(cucumber({}))
+      .pipe(process.chdir(cwd))
       .pipe(plugins.if(growl, plugins.notify({message: "Cucumber client task completed."})));
   });
 };
